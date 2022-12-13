9efb57fb48750079819dad500de35b4e9de6c7db
shobrook <shobrookj@gmail.com>
2022-12-13 01:16:06 -0500

    Organized backend

--- adrenaline/public/electron.js
+++ adrenaline/public/electron.js
@@ -1,47 +1,56 @@
-const path = require('path');
+/***********
+ * Constants
+ ***********/
 
-const { app, BrowserWindow } = require('electron');
-const isDev = require('electron-is-dev');
+const { app, BrowserWindow, ipcMain } = require("electron");
+const path = require("path");
+const url = require("url");
+const isDev = require("electron-is-dev");
+
+/**************
+ * Window Setup
+ **************/
+
+let mainWindow;
 
 function createWindow() {
-  // Create the browser window.
-  const win = new BrowserWindow({
-    width: 800,
-    height: 600,
-    webPreferences: {
-      nodeIntegration: true,
-    },
-  });
+	// Create the browser window.
+	mainWindow = new BrowserWindow({
+		width: 400,
+		height: 600,
+		resizable: false,
+		// titleBarStyle: "hidden",
+		webPreferences: {
+			nodeIntegration: true
+		}
+	});
 
-  // and load the index.html of the app.
-  // win.loadFile("index.html");
-  win.loadURL(
-    isDev
-      ? 'http://localhost:3000'
-      : `file://${path.join(__dirname, '../build/index.html')}`
-  );
-  // Open the DevTools.
-  if (isDev) {
-    win.webContents.openDevTools({ mode: 'detach' });
-  }
+	mainWindow.loadURL(
+		isDev
+			? "http://localhost:3000"
+			: `file://${path.join(__dirname, "../build/index.html")}`
+	);
+	mainWindow.on("closed", () => (mainWindow = null));
+	mainWindow.webContents.openDevTools(); // TEMP: For testing
 }
 
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
-// Some APIs can only be used after this event occurs.
 app.whenReady().then(createWindow);
-
-// Quit when all windows are closed, except on macOS. There, it's common
-// for applications and their menu bar to stay active until the user quits
-// explicitly with Cmd + Q.
-app.on('window-all-closed', () => {
-  if (process.platform !== 'darwin') {
-    app.quit();
-  }
-});
-
 app.on('activate', () => {
   if (BrowserWindow.getAllWindows().length === 0) {
     createWindow();
   }
 });
+app.on("window-all-closed", () => {
+	if (process.platform !== "darwin") {
+		app.quit();
+	}
+});
+
+/******************************
+ * Inter-Process Event Handlers
+ ******************************/
+
+// ipcMain.on("fixErrorRequest", (event, arg) => {
+//   const { brokenCode, stackTrace } = arg; // brokenCode as {lineNo: lineOfCode}
+//   event.fixedCode = {};
+// });
