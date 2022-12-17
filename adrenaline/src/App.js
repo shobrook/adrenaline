962f4e48abd16ba3dca3c96a21b518b885423691
mbladra <mdrabla1@gmail.com>
2022-12-17 17:14:05 -0500

    Backend for running commands (python) and grabbing stdout/err

--- adrenaline/src/App.js
+++ adrenaline/src/App.js
@@ -57,6 +57,19 @@ export default class App extends Component {
   //   });
   // }
 
+  onRunCode = () => {
+    // Get code from state
+    ipcRenderer.sendSync("runCodeRequest", {
+      // TODO: Error handling for bad filename/paths
+      command: filePath + '/' + fileName
+    });
+    ipcRenderer.on("runCodeResponse", (event, arg) => {
+      const { stdOut, stdErr } = arg;
+      console.log(`stdout: ${stdOut}`);
+      console.log(`stderr: ${stdErr}`);
+    });
+  }
+
 	render() {
     const { fileName, filePath, codeEditor, code, screen } = this.state;
 
@@ -69,6 +82,7 @@ export default class App extends Component {
         </div>
       );
     } else if (screen == SCREENS.EditFile) {
+      let response = this.onRunCode();
       return (
         <div className="editFileScreen">
           <Header
