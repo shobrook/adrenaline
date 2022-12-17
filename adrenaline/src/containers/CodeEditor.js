7f40267f7e0a865aa7922be0b3fdd3cf7e81de31
shobrook <shobrookj@gmail.com>
2022-12-16 23:12:56 -0500

    Got a really jank version of the diff working; doesn't work when changes in the editor occur tho

--- adrenaline/src/containers/CodeEditor.js
+++ adrenaline/src/containers/CodeEditor.js
@@ -1,21 +1,62 @@
 import React, { Component, Fragment } from "react";
-import "./CodeEditor.css";
-
 import CodeMirror from '@uiw/react-codemirror';
-// import { EditorState, EditorView, basicSetup } from "@codemirror/basic-setup";
 import { python } from '@codemirror/lang-python';
 
+import "./CodeEditor.css";
+
 export default class CodeEditor extends Component {
+	constructor(props) {
+    super(props);
+
+    this.codeMirrorRef = React.createRef();
+	}
+
+	componentDidMount(props) {
+		const { codeChanges } = this.props;
+
+		let codeIsRendered = false;
+		const interval = setInterval(() => {
+			if (codeIsRendered) {
+				clearInterval(interval);
+			}
+
+			let linesOfCode = this.codeMirrorRef.current.editor.getElementsByClassName("cm-line");
+
+			if (linesOfCode.length == 0) {
+				return;
+			}
+
+			codeIsRendered = true;
+			codeChanges.forEach(codeChange => {
+				const { oldLines, newLines } = codeChange;
+
+				Array.from(linesOfCode).forEach((lineOfCode, index, _) => {
+					if (oldLines.includes(index)) {
+						lineOfCode.className = "cm-line oldLine";
+					} else if (newLines.includes(index)) {
+						lineOfCode.className = "cm-line newLine";
+					}
+				});
+			});
+	  }, 100);
+	}
+
 	render() {
-    const { code } = this.props;
-		const stringifiedCode = code.join("\n");
+    const { code, codeChanges } = this.props;
+
 
     return (
+			<Fragment>
       <CodeMirror
+				ref={this.codeMirrorRef}
         className="editor"
-        value={stringifiedCode}
-        extensions={[python()]}
+        value={code.join("\n")}
+				extensions={[python()]}
+        onChange={(editor, data, value) => {
+          this.setState({});
+        }}
       />
-    )
+			</Fragment>
+    );
 	}
 }
