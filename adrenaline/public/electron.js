807ddf07df634f975477b3c331e45834baca7e51
shobrook <shobrookj@gmail.com>
2022-12-18 15:09:41 -0500

    Fixed run command stuff

--- adrenaline/public/electron.js
+++ adrenaline/public/electron.js
@@ -28,22 +28,16 @@ const buildGPTPrompt = (brokenCode, stackTrace) =>
 	 ${defaultConfig.solutionKey}`;
 
 const parseGPTOutput = (brokenCode, fixedCode) => {
-	console.log("broken code: \n", brokenCode)
-
 	const diff = Diff.diffTrimmedLines(brokenCode, fixedCode);
 	let mergedCode = "";
 
 	for (let i = 0; i < diff.length; i++) {
 		let part = diff[i];
 
-		console.log("diff value: ", part.value)
-		console.log("diff added: ", part.added)
-		console.log("diff removed: ", part.removed)
-
 		if (!part.added && !part.removed) {
 			mergedCode += part.value;
 		} else if (part.removed) {
-			mergedCode += '>>>OLD CODE<<<\n';
+			mergedCode += '>>> OLD CODE\n';
 
 			for (let j = 0; j < part.value.length; j++) {
 				mergedCode += part.value[j];
@@ -55,11 +49,9 @@ const parseGPTOutput = (brokenCode, fixedCode) => {
 				mergedCode += part.value[j];
 			}
 
-			mergedCode += '>>>FIXED CODE<<<\n'
+			mergedCode += '>>> FIXED CODE\n'
 		}
 	}
-
-	console.log("final merged code: ", mergedCode)
 
 	const codeChanges = [];
 	const lines = mergedCode.split('\n');
@@ -70,20 +62,20 @@ const parseGPTOutput = (brokenCode, fixedCode) => {
 
 	for (let i = 0; i < lines.length; i++) {
 		const line = lines[i];
-		if (line.includes('>>>OLD CODE<<<')) {
+		if (line.includes('>>> OLD CODE')) {
 			oldLines.push(i);
-		} else if (line.includes('>>>FIXED CODE<<<')) {
+		} else if (line.includes('>>> FIXED CODE')) {
 			newLines.push(i);
 		} else if (line.includes('===============')) {
 			mergeLine = i;
 		}
 
 		if (oldLines.length > 0 && newLines.length > 0 && mergeLine > -1) {
-			for (let j = oldLines[0]+1; j < mergeLine; j++) {
+			for (let j = oldLines[0] + 1; j < mergeLine; j++) {
 				oldLines.push(j);
 			}
 
-			for (let k = mergeLine+1; k < newLines[0]; k++) {
+			for (let k = mergeLine + 1; k < newLines[0]; k++) {
 				newLines.push(k);
 			}
 
@@ -93,12 +85,6 @@ const parseGPTOutput = (brokenCode, fixedCode) => {
 			mergeLine = -1;
 		}
 	}
-
-	codeChanges.forEach( (codeChange) => {
-		console.log("\tlines added: ", codeChange.newLines)
-		console.log("\tlines removed: ", codeChange.oldLines)
-		console.log("\tmed line: ", codeChange.mergeLine)
-	});
 
 	return {
 		mergedCode,
@@ -155,27 +141,27 @@ ipcMain.on("runCommandRequest", (event, arg) => {
 
 	if (command.trim() === "") {
 		event.reply("runCommandResponse", { command, stdout: "", stderr: "" });
-		return;
-	}
-
-	let commandToRun;
-	let commandParts = command.split(" ");
-	if (commandParts.length > 1) {
-		const homeDir = os.homedir();
-		const relativePath = `${currDir}/${commandParts[1]}`
-		const absolutePath = path.resolve(homeDir, relativePath);
-
-		commandToRun = `${commandParts[0]} ${absolutePath} ${commandParts.slice(2, commandParts.length)}`;
 	} else {
-		commandToRun = command;
-	}
+		let commandToRun;
+		let commandParts = command.split(" ");
+		if (commandParts.length > 1) {
+			const homeDir = os.homedir();
+			const relativePath = `${currDir}/${commandParts[1]}`
+			const absolutePath = path.resolve(homeDir, relativePath);
 
-	exec(commandToRun, (err, stdout, stderr) => {
-	  event.reply("runCommandResponse", {command, stdout, stderr});
-	});
+			commandToRun = `${commandParts[0]} ${absolutePath} ${commandParts.slice(2, commandParts.length)}`;
+		} else {
+			commandToRun = command;
+		}
+
+		exec(commandToRun, (err, stdout, stderr) => {
+		  event.reply("runCommandResponse", {command, stdout, stderr});
+		});
+	}
 });
 
 ipcMain.on("fixErrorRequest", (event, arg) => {
+	console.log("RECEIEVED fixErrorRequest");
 	let testBrokenCodeStr = `
 		x = 10
 		y = "hey"
@@ -191,25 +177,9 @@ ipcMain.on("fixErrorRequest", (event, arg) => {
 		print(result)
 	`
 	const { mergedCode, codeChanges } = parseGPTOutput(testBrokenCodeStr, testFixedCodeStr);
+	console.log("sent reply")
 	event.reply("fixErrorResponse", { mergedCode, codeChanges });
-	return;
-
-  const { code, stackTrace } = arg;
-	const prompt = buildGPTPrompt(code, stackTrace);
-	const apiConfig = new Configuration({apiKey: defaultConfig.apiKey});
-	const api = new OpenAIApi(apiConfig);
-
-
-	api
-		.createCompletion({
-	    prompt,
-	    ...defaultConfig.completionPromptParams
-	  })
-	  .then(data => {
-
-		})
-		.catch((error) => console.log(error.response));
-});
+})
 
 ipcMain.on("saveFileRequest", (event, arg) => {
 	const { code, filePath } = arg;
@@ -218,12 +188,9 @@ ipcMain.on("saveFileRequest", (event, arg) => {
 
 	fs.writeFile(fullPath, code.join("\n"), { flag: 'wx' }, err => {
 		if (err) {
-			console.log(filePath);
-			console.log(err);
 			event.reply("saveFileResponse", { success: false });
 		} else {
 			event.reply("saveFileResponse", { success: true })
 		}
-	});
-
-})
+ 	});
+});
