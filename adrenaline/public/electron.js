cfb1a040faf4a916512b0729f14a229718130f50
shobrook <shobrookj@gmail.com>
2022-12-18 13:57:13 -0500

    Wired up GPT output parsing to renderer

--- adrenaline/public/electron.js
+++ adrenaline/public/electron.js
@@ -11,7 +11,8 @@ const os = require("os");
 const { Configuration, OpenAIApi } = require("openai");
 const { exec, spawn } = require('node:child_process');
 const defaultConfig = require('./config.js');
-//const diff = require('diff');
+const Diff = require("diff");
+
 /*********
  * Helpers
  *********/
@@ -25,6 +26,85 @@ const buildGPTPrompt = (brokenCode, stackTrace) =>
 	 ${stackTrace}
 
 	 ${defaultConfig.solutionKey}`;
+
+const parseGPTOutput = (brokenCode, fixedCode) => {
+	console.log("broken code: \n", brokenCode)
+
+	const diff = Diff.diffTrimmedLines(brokenCode, fixedCode);
+	let mergedCode = "";
+
+	for (let i = 0; i < diff.length; i++) {
+		let part = diff[i];
+
+		console.log("diff value: ", part.value)
+		console.log("diff added: ", part.added)
+		console.log("diff removed: ", part.removed)
+
+		if (!part.added && !part.removed) {
+			mergedCode += part.value;
+		} else if (part.removed) {
+			mergedCode += '>>>OLD CODE<<<\n';
+
+			for (let j = 0; j < part.value.length; j++) {
+				mergedCode += part.value[j];
+			}
+
+			mergedCode += "================\n"
+		} else if (part.added) {
+			for (let j = 0; j < part.value.length; j++) {
+				mergedCode += part.value[j];
+			}
+
+			mergedCode += '>>>FIXED CODE<<<\n'
+		}
+	}
+
+	console.log("final merged code: ", mergedCode)
+
+	const codeChanges = [];
+	const lines = mergedCode.split('\n');
+
+	let oldLines = [];
+	let newLines = [];
+	let mergeLine = -1;
+
+	for (let i = 0; i < lines.length; i++) {
+		const line = lines[i];
+		if (line.includes('>>>OLD CODE<<<')) {
+			oldLines.push(i);
+		} else if (line.includes('>>>FIXED CODE<<<')) {
+			newLines.push(i);
+		} else if (line.includes('===============')) {
+			mergeLine = i;
+		}
+
+		if (oldLines.length > 0 && newLines.length > 0 && mergeLine > -1) {
+			for (let j = oldLines[0]+1; j < mergeLine; j++) {
+				oldLines.push(j);
+			}
+
+			for (let k = mergeLine+1; k < newLines[0]; k++) {
+				newLines.push(k);
+			}
+
+			codeChanges.push({ oldLines, newLines, mergeLine });
+			oldLines = [];
+			newLines = [];
+			mergeLine = -1;
+		}
+	}
+
+	codeChanges.forEach( (codeChange) => {
+		console.log("\tlines added: ", codeChange.newLines)
+		console.log("\tlines removed: ", codeChange.oldLines)
+		console.log("\tmed line: ", codeChange.mergeLine)
+	});
+
+	return {
+		mergedCode,
+		codeChanges
+	};
+}
 
 /**************
  * Window Setup
@@ -96,10 +176,29 @@ ipcMain.on("runCommandRequest", (event, arg) => {
 });
 
 ipcMain.on("fixErrorRequest", (event, arg) => {
+	let testBrokenCodeStr = `
+		x = 10
+		y = "hey"
+		z = "yo"
+		result = x + y + z
+		print("What's up")
+	`
+	let testFixedCodeStr = `
+		x = 10
+		y = 20
+		z = 30
+		result = x + y + z
+		print(result)
+	`
+	const { mergedCode, codeChanges } = parseGPTOutput(testBrokenCodeStr, testFixedCodeStr);
+	event.reply("fixErrorResponse", { mergedCode, codeChanges });
+	return;
+
   const { code, stackTrace } = arg;
 	const prompt = buildGPTPrompt(code, stackTrace);
 	const apiConfig = new Configuration({apiKey: defaultConfig.apiKey});
 	const api = new OpenAIApi(apiConfig);
+
 
 	api
 		.createCompletion({
@@ -107,8 +206,7 @@ ipcMain.on("fixErrorRequest", (event, arg) => {
 	    ...defaultConfig.completionPromptParams
 	  })
 	  .then(data => {
-			//diffTool(code, data.data.choices[0].text.split("\n"))
-			event.reply("fixErrorResponse", {fixedCode: data.data.choices[0].text.split("\n")});
+
 		})
 		.catch((error) => console.log(error.response));
 });
--- adrenaline/src/App.js
+++ adrenaline/src/App.js
@@ -4,7 +4,6 @@ import OpenFileButton from "./components/OpenFileButton";
 import Header from "./containers/Header";
 import CodeEditor from "./containers/CodeEditor";
 import Terminal from "./containers/Terminal";
-import * as Diff from 'diff';
 
 import './App.css';
 
@@ -87,121 +86,7 @@ export default class App extends Component {
 
   onFixCode = () => {
     const { code, stderr } = this.state;
-    console.log("onfixcode")
 
-    //Testing
-    var testBrokenCodeStr = `
-      def apply_input_to_func(func, input):
-          func(input)
-
-      def main():
-          my_data = []
-          their_data = []
-          for i in range(10):
-              apply_input_to_func(my_data.append, i)
-              their_data.add(i)
-
-          print(my_data)
-
-      main()
-    `
-    testBrokenCodeStr = `
-      x = 10
-      y = "hey"
-      z = "yo"
-      result = x + y + z
-      print("What's up")
-    `
-    var testFixedCodeStr = `
-      def apply_input_to_func(func, input):
-          func(input)
-          // this is some new shit
-
-      def main():
-          my_data = []
-          for i in range(10):
-              apply_input_to_func(my_data.append, i)
-
-          print(my_data)
-
-      main()
-    `
-    testFixedCodeStr = `
-      x = 10
-      y = 20
-      z = 30
-      result = x + y + z
-      print(result)
-    `
-    console.log("broken code: \n", testBrokenCodeStr)
-    //const { mergedCodeStr, codeChanges } = this.diffCode(testBrokenCodeStr, testFixedCodeStr);
-    const diff = Diff.diffTrimmedLines(testBrokenCodeStr, testFixedCodeStr);
-    var mergedCode = ""
-
-
-    for(let i=0; i< diff.length; i++){
-      let part = diff[i];
-      console.log("diff value: ", part.value)
-      console.log("diff added: ", part.added)
-      console.log("diff removed: ", part.removed)
-
-      if (!part.added && !part.removed) {
-        mergedCode += part.value;
-
-      } else if (part.removed) {
-
-        mergedCode += '>>>OLD CODE<<<\n';
-
-        for (let j = 0; j < part.value.length; j++) {
-          mergedCode += part.value[j];
-        }
-        mergedCode += "================\n"
-
-      } else if (part.added) {
-        for (let j = 0; j < part.value.length; j++) {
-          mergedCode += part.value[j];
-        }
-        mergedCode += '>>>FIXED CODE<<<\n'
-
-      }
-    }
-    console.log("final merged code: ", mergedCode)
-    const codeChanges = [];
-    const lines = mergedCode.split('\n');
-
-    let oldLines = [];
-    let newLines = [];
-    let mergeLine = -1;
-
-    for (let i = 0; i < lines.length; i++) {
-      const line = lines[i];
-      if (line.includes('>>>OLD CODE<<<')) {
-        oldLines.push(i);
-      } else if (line.includes('>>>FIXED CODE<<<')) {
-        newLines.push(i);
-      } else if (line.includes('===============')) {
-        mergeLine = i;
-      }
-
-      if (oldLines.length > 0 && newLines.length > 0 && mergeLine > -1) {
-        for (let j = oldLines[0]+1; j < mergeLine; j++) {
-          oldLines.push(j);
-        }
-        for (let k = mergeLine+1; k < newLines[0]; k++) {
-          newLines.push(k);
-        }
-        codeChanges.push({ oldLines, newLines, mergeLine });
-        oldLines = [];
-        newLines = [];
-        mergeLine = -1;
-      }
-    }
-
-    codeChanges.forEach( (codeChange) => {
-  		console.log("\tlines added: ", codeChange.newLines)
-  		console.log("\tlines removed: ", codeChange.oldLines)
-  		console.log("\tmed line: ", codeChange.mergeLine)
-    })
     ipcRenderer.sendSync("fixErrorRequest", {
       code: code,
       stackTrace: stderr
@@ -239,39 +124,9 @@ export default class App extends Component {
     });
 
     ipcRenderer.on("fixErrorResponse", (event, arg) => {
-      const { fixedCodeStr } = arg;
-      //Testing
-      const testBrokenCodeStr = `
-        def apply_input_to_func(func, input):
-            func(input)
+      const { mergedCode, codeChanges } = arg;
 
-        def main():
-            my_data = []
-            their_data = []
-            for i in range(10):
-                apply_input_to_func(my_data.append, i)
-                their_data.add(i)
-
-            print(my_data)
-
-        main()
-      `
-      const testFixedCodeStr = `
-        def apply_input_to_func(func, input):
-            func(input)
-            // this is some new shit
-
-        def main():
-            my_data = []
-            for i in range(10):
-                apply_input_to_func(my_data.append, i)
-
-            print(my_data)
-
-        main()
-        `
-      console.log("broken code: \n", testBrokenCodeStr)
-
+      this.setState({ code: mergedCode.split("\n"), codeChanges });
     });
 
     ipcRenderer.on("saveFileResponse", (event, arg) => {
