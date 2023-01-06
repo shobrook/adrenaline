44e5b264b83155d34d2264883fc43359325c76ea
shobrook <shobrookj@gmail.com>
2023-01-05 23:53:27 -0500

    Attempt at fixing homepage + react router issue

--- adrenaline/src/index.js
+++ adrenaline/src/index.js
@@ -1,26 +1,37 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { createHashRouter, RouterProvider } from "react-router-dom";
+import { HashRouter as Router, Route } from 'react-router-dom';
 
 import './index.css';
 
 import Landing from "./routes/Landing";
 import App from "./routes/App";
 
-const router = createHashRouter([
-  {
-    path: "/",
-    element: <Landing />
-  },
-  {
-    path: "/playground",
-    element: <App />
-  }
-]);
+// const router = createHashRouter([
+//   {
+//     path: "/",
+//     element: <Landing />
+//   },
+//   {
+//     path: "/playground",
+//     element: <App />
+//   }
+// ]);
+//
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <RouterProvider router={router} />
+//   </React.StrictMode>,
+//   document.getElementById("root")
+// );
 
 ReactDOM.render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+  <Router basename="https://shobrook.github.io/adrenaline">
+    <Route exact path="/" component={Landing} />
+    <Route path="/playground" component={App} />
+  </Router>
   </React.StrictMode>,
   document.getElementById("root")
 );
