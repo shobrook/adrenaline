5f5c9a94a27e5677210f71386b3225be38f46c0b
shobrook <shobrookj@gmail.com>
2023-01-06 00:19:39 -0500

    Reverted all the GH pages bullshit; using Heroku now

--- adrenaline/src/index.js
+++ adrenaline/src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { createHashRouter, RouterProvider } from "react-router-dom";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import { HashRouter as Router, Route } from 'react-router-dom';
 
 import './index.css';
@@ -8,30 +8,20 @@ import './index.css';
 import Landing from "./routes/Landing";
 import App from "./routes/App";
 
-// const router = createHashRouter([
-//   {
-//     path: "/",
-//     element: <Landing />
-//   },
-//   {
-//     path: "/playground",
-//     element: <App />
-//   }
-// ]);
-//
-// ReactDOM.render(
-//   <React.StrictMode>
-//     <RouterProvider router={router} />
-//   </React.StrictMode>,
-//   document.getElementById("root")
-// );
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Landing />
+  },
+  {
+    path: "/playground",
+    element: <App />
+  }
+]);
 
 ReactDOM.render(
   <React.StrictMode>
-  <Router basename="https://shobrook.github.io/adrenaline">
-    <Route exact path="/" component={Landing} />
-    <Route path="/playground" component={App} />
-  </Router>
+    <RouterProvider router={router} />
   </React.StrictMode>,
   document.getElementById("root")
 );
