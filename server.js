4d4bfae2fdf55ed845f0e85ac7544879f61fe216
shobrook <shobrookj@gmail.com>
2023-01-17 15:26:26 -0500

    Removed wildcard GET

--- server.js
+++ server.js
@@ -12,8 +12,12 @@ const port = process.env.PORT || 3000;
 app.use(cors());
 app.use(express.static(publicPath));
 
-app.get('*', (req, res) => {
-res.sendFile(path.join(publicPath, 'index.html'));
+app.get("/", (req, res) => {
+  res.sendFile(path.join(publicPath, 'index.html'));
+});
+
+app.get("/playground", (req, res) => {
+  res.sendFile(path.join(publicPath, 'index.html'));
 });
 
 app.listen(port, () => {
