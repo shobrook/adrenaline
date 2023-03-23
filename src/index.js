692bb45f003a7a81fc49ae2bdc46e23a867ff81f
shobrook <shobrookj@gmail.com>
2023-03-22 18:23:41 -0700

    Added payment stuff

--- src/index.js
+++ src/index.js
@@ -8,6 +8,7 @@ import Auth0ProviderWithHistory from "./auth/Auth0ProviderWithHistory";
 import Landing from "./routes/Landing";
 import App from "./routes/App";
 import Subscription from './routes/Subscription';
+import ApiSubscription from "./routes/ApiSubscription";
 
 ReactDOM.render(
   <React.StrictMode>
@@ -17,6 +18,7 @@ ReactDOM.render(
           <Route path="/" exact element={<Landing />} />
           <Route path="/playground" element={<App />} />
           <Route path="/subscription" element={<Subscription />} />
+          <Route path="/apisubscription" element={<ApiSubscription />} />
         </Routes>
       </Auth0ProviderWithHistory>
     </BrowserRouter>
---
+++ src/routes/ApiSubscription.js
@@ -0,0 +1,96 @@
+import { Component } from "react";
+import { withAuth0 } from "@auth0/auth0-react";
+
+import Button from "../components/Button";
+import { withRouter } from "../library/utilities";
+
+import "../styles/ApiSubscription.css";
+import "../styles/PaymentPlan.css";
+
+class ApiSubscription extends Component {
+    onSubscribe = () => {
+        const {
+            isAuthenticated,
+            loginWithRedirect,
+            getAccessTokenSilently,
+            user
+        } = this.props.auth0;
+
+        if (!isAuthenticated) {
+            loginWithRedirect({
+                appState: {
+                    returnTo: window.location.pathname
+                }
+            });
+            return;
+        }
+
+        getAccessTokenSilently()
+            .then(token => {
+                fetch("https://staging-rubrick-api-production.up.railway.app/api/stripe/create_checkout_session", {
+                    method: "POST",
+                    headers: {
+                        "Content-Type": "application/json",
+                        "Authorization": `Bearer ${token}`
+                    },
+                    body: JSON.stringify({
+                        user_id: user.sub,
+                        lookup_key: "api_access"
+                    })
+                })
+                    .then(res => {
+                        console.log(res)
+                        return res.json()
+                    })
+                    .then(data => {
+                        const { checkout_url } = data;
+
+                        const win = window.open(checkout_url, "_blank");
+                        if (win != null) {
+                            win.focus();
+                        }
+                    })
+            })
+    }
+
+    renderFeatures() {
+        const isSelected = true;
+        const features = [
+            "Up to 500 requests to the debugging endpoint"
+        ]
+
+        return features.map(feature => {
+            return (
+                <div className="planFeature">
+                    <img src={isSelected ? "./checkmark_primary.png" : "./checkmark_secondary.png"} alt={"checkmark"} />
+                    <span>{feature}</span>
+                </div>
+            );
+        });
+    }
+
+    render() {
+        const isSelected = true;
+
+        return (
+            <div id="apiSubscription">
+                <div className={`paymentPlan ${isSelected ? "isSelected" : ""}`}>
+                    <span className={`planLabel ${isSelected ? "isSelected" : ""}`}>API Access</span>
+                    <span className="planPrice">$50/month</span>
+                    <Button
+                        className={isSelected ? "selectPlanPrimary" : "selectPlan"}
+                        isPrimary={isSelected}
+                        onClick={this.onSubscribe}
+                    >
+                        Get started
+                    </Button>
+                    <div className={`planFeatures ${isSelected ? "isSelected" : ""}`}>
+                        {this.renderFeatures()}
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default withAuth0(ApiSubscription);
